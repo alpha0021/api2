30baa4e217ec11ced916d9e3326bbfe384f06e9a
rushdi <rushdi@gmail.com>
2017-08-02 23:18:04 +0200

    2 files modified for showing  error to client

--- routes/api.js
+++ routes/api.js
@@ -3,31 +3,31 @@ const router = express.Router();
 const Ninja = require('../models/ninja');
 
 //get a list of ninjas  from  the database
-router.get("/ninjas",function(req,res){
+router.get("/ninjas",function(req,res,next){
 res.send({type:"GET"});
 });
 
 
 //add a new ninja
-router.post("/ninjas",function(req,res){
+router.post("/ninjas",function(req,res,next){
 
 	Ninja.create(req.body).then(function(ninja){
 
 		res.send(ninja);
 
-	});
+	}).catch(next);
 
 });
 
 
 //update ninja
-router.put("/ninjas/:id",function(req,res){
+router.put("/ninjas/:id",function(req,res,next){
 res.send({type:"PUT"});
 });
 
 
 //delete ninja from database
-router.delete("/ninjas/:id",function(req,res){
+router.delete("/ninjas/:id",function(req,res,next){
 res.send({type:"DELETE"});
 });
 
